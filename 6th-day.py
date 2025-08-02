03b3b87eb3786e239e181b77cd2b9113c601e790
Istiyaq-Khan <istiyaqkhanr@gmail.com>
2025-08-02 14:37:45 +0600

    Update 6th-day.py

--- 6th-day.py
+++ 6th-day.py
@@ -7,7 +7,7 @@ while i < 5:
 
 # the starting, stoping , and stepping argument to range()
 
-for i in range(12, 16): # the frist argument will be where the for loop variable start and the 2nd argument will be up to, but not including, the number to stop at
+for i in range(12, 16):  # the frist argument will be where the for loop variable start and the 2nd argument will be up to, but not including, the number to stop at
     print(i)
 
 for i in range(0, 12, 2):
@@ -17,4 +17,3 @@ for i in range(0, 12, 2):
 for i in range(5, -1, -1):
     print(i)
     # running a for loop to print i with range(5, -1, -1) should print from five down to zero.
-
